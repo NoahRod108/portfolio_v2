04a213a11b9d9d9bc37528030db4bc6e65d7504d
Noah Rodriguez <nrodrig3@valpo.edu>
2023-09-07 16:51:08 -0500

    Fixed footer width

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='max-container px-4 flex flex-col xl:w-[450px] justify-center items-center text-dark-gray text-sm gap-4 py-8 text-center'>
+    <footer className='max-container px-4 flex flex-col xl:w-[464px] justify-center items-center text-dark-gray text-sm gap-4 py-8 text-center'>
         <div>
             Designed & Built  by Noah Rodriguez
         </div>
